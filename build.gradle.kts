b966ed974400169cf19104d2130a2fd10c6123a2
Sascha Wiegleb <sascha.wiegleb@datameer.com>
2019-08-11 00:28:37 +0200

    cleanup

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,7 @@
 plugins {
     java
-
-    // Apply the application plugin to add support for building a CLI application
-    application
-
     jacoco
-
     id("org.sonarqube") version "2.7"
-
     eclipse
 }
 
@@ -18,31 +12,19 @@ repositories {
 }
 
 dependencies {
-    // This dependency is used by the application.
     implementation("com.google.guava:guava:27.1-jre")
-
-    // Use JUnit Jupiter API for testing.
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.4.2")
-
-    // Use JUnit Jupiter Engine for testing.
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.4.2")
-}
-
-application {
-    // Define the main class for the application
-    mainClassName = "saschawiegleb.App"
 }
 
 tasks {
     "test"(Test::class) {
-        // Use junit platform for unit tests
         useJUnitPlatform()
     }
 
     val codeCoverageReport by creating(JacocoReport::class) {
         executionData(fileTree(project.rootDir.absolutePath).include("**/build/jacoco/*.exec"))
 
-        // Add all relevant sourcesets from the subprojects
         subprojects.onEach {
             sourceSets(it.sourceSets["main"])
         }
